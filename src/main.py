a78c8356db21a6eb3fcf70427d7f6bbf59845826
cordea <cordear@outlook.com>
2021-07-18 22:29:41 +0800

    modify the log config

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import cv2
 import sys
+import os
 import logging
 if __name__ == "__main__":
     logFormat: str = "%(asctime)s - %(levelname)s: %(message)s"
-    logging.basicConfig(filename="./venv/log/log", format=logFormat,
-                        level=logging.DEBUG)
+    logpath: str = os.path.split(
+        os.path.split(os.path.realpath(__file__))[0])[0]
+    logging.basicConfig(format=logFormat,
+                        level=logging.DEBUG, filename="{0}/log/log".format(logpath))
     print(cv2.__version__)
-    print(sys.argv[0])
+    # print(logpath)
     video = cv2.VideoCapture(0)
     logging.info("Camera set for 0.")
     if(video.isOpened() != True):
@@ -15,6 +18,7 @@ if __name__ == "__main__":
         sys.exit()
     while(True):
         ret, frame = video.read()
+        frame = cv2.flip(frame, 0)  # Flip the picture. Change if needed
         cv2.imshow("camera", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             logging.info("Quit")

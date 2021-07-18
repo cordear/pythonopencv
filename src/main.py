27c347c533ae99b82773c518f6827da595ba6dfe
cordea <cordear@outlook.com>
2021-07-19 01:01:50 +0800

    add convexity defects detect

--- src/main.py
+++ src/main.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     # print(logpath)
 
     element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-
+    hull = []
+    hullI = []
     video = cv2.VideoCapture(0)
     logging.info("Camera set for 0.")
     if(video.isOpened() != True):
@@ -42,10 +43,38 @@ if __name__ == "__main__":
             skinFrame, cv2.MORPH_CLOSE, element, iterations=6)
         skinFrame = cv2.morphologyEx(
             skinFrame, cv2.MORPH_OPEN, element, iterations=6)
-        skinFrame = cv2.cvtColor(skinFrame, cv2.COLOR_GRAY2BGR)
+        contours, _ = cv2.findContours(
+            skinFrame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
+        skinFrame = cv2.cvtColor(
+            skinFrame, cv2.COLOR_GRAY2BGR)  # Extend frame layers
         skin = cv2.bitwise_and(frame, skinFrame)
         cv2.imshow("Origin", frame)
         cv2.imshow("Skin", skin)
+        for t in contours:
+            if cv2.contourArea(t) < 100:
+                continue
+            hull.append(cv2.convexHull(t))
+            hullI = cv2.convexHull(t, clockwise=True, returnPoints=False)
+            defects = cv2.convexityDefects(t, hullI)
+            if defects is None:
+                continue
+            for i in range(defects.shape[0]):
+                s, e, f, d = defects[i, 0]
+                d = d/256
+                if(d <= 40 or d >= 150):
+                    continue
+                start = t[s][0]
+                end = t[e][0]
+                far = t[f][0]
+                cv2.line(frame, start, far, (0, 255, 0), 2)
+                cv2.line(frame, end, far, (0, 255, 0), 2)
+                cv2.circle(frame, start, 6, (255, 0, 0))
+                cv2.circle(frame, end, 6, (0, 255, 0))
+                cv2.circle(frame, far, 6, (0, 0, 255))
+
+        frame = cv2.drawContours(frame, hull, -1, (255, 0, 0))
+        cv2.imshow("Final", frame)
+        hull.clear()
         if cv2.waitKey(1) & 0xFF == ord("q"):
             logging.info("Quit")
             break
